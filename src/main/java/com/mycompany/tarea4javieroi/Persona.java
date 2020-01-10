99ac5c40e618aae232a91c09bcec53eee06e72f1
ocagnen <ocagnen2@gmail.com>
2020-01-10 18:36:05 +0100

    Añade toString en clase modelo

--- src/main/java/com/mycompany/tarea4javieroi/Persona.java
+++ src/main/java/com/mycompany/tarea4javieroi/Persona.java
@@ -37,6 +37,11 @@ public class Persona {
     public void setEdad(int edad) {
         this.edad = edad;
     }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", edad=" + edad + '}';
+    }
     
     
     
