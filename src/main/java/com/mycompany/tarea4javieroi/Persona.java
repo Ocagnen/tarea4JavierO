ea944b9dc5c8d067716673b113358edc15420812
ocagnen <ocagnen2@gmail.com>
2020-01-10 18:32:05 +0100

    Añade constructores en clase modelo

--- src/main/java/com/mycompany/tarea4javieroi/Persona.java
+++ src/main/java/com/mycompany/tarea4javieroi/Persona.java
@@ -13,5 +13,15 @@ public class Persona {
     
     private String nombre;
     private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public Persona() {
+    }
+    
+    
     
 }
