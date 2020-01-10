ea1d74ff790bc074b27df3393725a0faf7506e3d
jriosaguilar <josemanueljbk99@gmail.com>
2020-01-10 18:42:49 +0100

    Añade main y un par de objetos

--- src/main/java/com/mycompany/tarea4javieroi/Persona.java
+++ src/main/java/com/mycompany/tarea4javieroi/Persona.java
@@ -44,5 +44,13 @@ public class Persona {
     }
     
     
-    
+    public static void main(String[] args) {
+        
+        Persona persona1=new Persona();
+        Persona persona2=new Persona("Jose", 20);
+        
+        System.out.println(persona1);
+        System.out.println(persona2);
+        
+    }
 }
